cbe7c010d89e38a1ae22491496514bd9a8a21654
Chi Wang <chiwang@mail.ustc.edu.cn>
2013-11-14 17:44:56 +0800

    modified:   Tools/SoftwareSetup.sh

--- Tools/SoftwareSetup.sh
+++ Tools/SoftwareSetup.sh
@@ -4,15 +4,15 @@
 #=======================================#
 
 ##  set path
-ToolPath=/home/chi/Tools
+ToolPath=$HOME/Tools
 
 #+  git
 GIT_VERSION="1.8.4"         # which version you want to use
 if [[ `git --version 2>/dev/null | grep "GIT_VERSION"` == "" ]];then
-    source $HOME/share/login/git-completion.zsh
     GITSYS=$ToolPath/git_v$GIT_VERSION
     export PATH=$GITSYS/bin:$PATH
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GITSYS/lib:$GITSYS/lib64
+    source $HOME/share/login/git-completion.zsh
 fi
 
 #+  DMPSW (DAMPE software)
