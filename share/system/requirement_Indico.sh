db9980e0f0df2b7c7f8ebc61c4cb79ce3b0822d5
Chi Wang <chiwang@mail.ustc.edu.cn>
2013-12-23 22:53:39 +0800

    renamed:    bin/del -> bin/delete modified:   share/login/aliasrc new file:   share/system/requirement_Indico.sh renamed:    share/system/requirments_root.sh -> share/system/requirements_root.sh

---
+++ share/system/requirement_Indico.sh
@@ -0,0 +1,10 @@
+
+yum install -y python-devel httpd mod_wsgi php php-devel php-mysql python-repoze-tm2 python-transaction
+
+easy_install pip
+pip install PIL reportlab
+easy_install indico
+
+# untar pip.xx.tar.gz
+# cd into pip.xxx
+# python setup.py install (as root)
