a6017000a7555165bb1d4ea2e1b12cf660f5993c
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-01-06 10:44:29 +0800

    modified:   share/common/SConscript modified:   share/common/mainTemplate.cc

--- share/common/mainTemplate.cc
+++ share/common/mainTemplate.cc
@@ -2,18 +2,29 @@
 #ifdef DEBUG
 #include <assert.h>
 #endif
-
-
-//------------------------------------------------------------------------------
-void Usage(){
-  std::cout<<"\tUsage:\t"<<std::endl;
-}
+#include <boost/program_options.hpp>
 
 //------------------------------------------------------------------------------
 int main(int argc, char* argv[]){
-  std::cout<<"\n\tProject \"MyX\" in main function"<<std::endl;
-  Usage();
-  assert(argc == 2);
+//  ------> Program_options
+  boost::program_options::options_description options("Usage: MyX [options] \nOptions");
+
+  options.add_options()
+    ("help,h","Use -h or --help to display this information")
+    ("version,v","display version information")
+    ("filename",boost::program_options::value<std::string>(),"to find file");
+
+  boost::program_options::variables_map vmap;
+  boost::program_options::store(parse_command_line(argc,argv,options),vmap);
+  boost::program_options::notify(vmap);
+
+  if (vmap.count("help")) {
+    std::cout<<options<<std::endl;
+    return 0;
+  } else if (vmap.count("version")) {
+    std::cout<<"MyX Version = 0.21.8"<<std::endl;
+  }
+//  ------> Program_options
 
   return 0;
 }
