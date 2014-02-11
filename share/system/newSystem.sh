1e4a5a259b6d662ea7b3d7844112c9d17d072081
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-02-11 13:30:10 +0800

    modified:   share/system/newSystem.sh new file:   share/system/svnconfig

--- share/system/newSystem.sh
+++ share/system/newSystem.sh
@@ -4,6 +4,7 @@
 yum install -y zsh yum-fastestmirror
 yum install -y expat-devel
 yum install  -y xorg-x11-fonts-ISO8859-1-75dpi
+yum install -y colordiff
 yum -y update
 bash requirments_root.sh
 yum clean all
