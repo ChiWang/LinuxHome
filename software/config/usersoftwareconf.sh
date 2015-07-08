a1f9bdec3676a88cdeb3bb5448d1ae4b2fef6985
Chi Wang <chiwang@mail.ustc.edu.cn>
2015-07-08 16:16:43 +0800

    modified:   software/config/usersoftwareconf.sh

--- software/config/usersoftwareconf.sh
+++ software/config/usersoftwareconf.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 :<<EOF
- *  $Id: usersoftwareconf.sh, 2014-02-15 23:11:54 chi $
+ *  $Id: usersoftwareconf.sh, 2015-07-08 16:12:36 DAMPE $
  *  Author(s):
  *    Chi WANG (chiwang@mail.ustc.edu.cn) 24/10/2013
  *  Note:
@@ -8,17 +8,16 @@
 EOF
 
 # path
-userTool=$HOME/software
+#userTool=$HOME/software
 
 #+  DMPSW (DAMPE software)
 #source $HOME/main/DmpSW/svn/trunk/thisdmpsw.sh
-source $HOME/tmp/DmpInstall/bin/thisdmpsw.sh >&/dev/null
+#source $HOME/tmp/DmpInstall/bin/thisdmpsw.sh >&/dev/null
+source /exports/DMPSW/bin/thisdmpsw.sh
 
 #+  Set OpenMP
 if [ !1 ];then
-
-
 fi
 
-unset userTool
+#unset userTool
 
